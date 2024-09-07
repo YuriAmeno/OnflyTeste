0f90335d064bb4ecf07d0846ab491e011218702d
Yuri Ameno <amenoyuri1@gmail.com>
2024-09-07 15:26:26 -0300

    add policy task

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -2,15 +2,39 @@
 
 namespace App\Policies;
 
+use Illuminate\Auth\Access\Response;
+use App\Models\Task;
 use App\Models\User;
 
 class TaskPolicy
 {
+
     /**
-     * Create a new policy instance.
+     * Determine whether the user can view the model.
      */
-    public function __construct()
+    public function viewTask(User $user, Task $task): bool
     {
-        //
+        return $user->id === $task->user_id;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function updateTask(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
+    }
+
+    public function sendEmail(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function deleteTask(User $user, Task $task): bool
+    {
+        return $user->id === $task->user_id;
     }
 }
